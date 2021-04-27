fd5e42e8c0a1879bcb7ebffe7a8425c50f2984d6
Connor Boyle <connor.bo@gmail.com>
2021-04-27 08:42:36 -0700

    Reformat source Files

--- client/src/components/AnnotationView.js
+++ client/src/components/AnnotationView.js
@@ -1,39 +1,56 @@
-import { Paper, Zoom } from '@material-ui/core';
-import { DataGrid } from '@material-ui/data-grid';
+import {Paper} from '@material-ui/core';
+import {DataGrid} from '@material-ui/data-grid';
 import React from 'react';
-import { deidentificationStates } from './DeidentifiedText';
+import {deidentificationStates} from './DeidentifiedText';
+import PropTypes from 'prop-types';
 
-export function AnnotationView(props) {
-
+function AnnotationView(props) {
   const types = [
-    {type: "text_date", name: "Date", key: "textDateAnnotations"},
-    {type: "text_physical_address", name: "Physical Address", key: "textPhysicalAddressAnnotations"},
-    {type: "text_person_name", name: "Person Name", key: "textPersonNameAnnotations"}
-  ]
+    {type: 'text_date', name: 'Date', key: 'textDateAnnotations'},
+    {type: 'text_physical_address', name: 'Physical Address', key:
+    'textPhysicalAddressAnnotations'},
+    {type: 'text_person_name', name: 'Person Name', key:
+    'textPersonNameAnnotations'},
+  ];
 
   let allAnnotations;
-  if (props.annotations === deidentificationStates.EMPTY || props.annotations === deidentificationStates.LOADING || props.annotations === deidentificationStates.ERROR) {
-    allAnnotations = []
+  if (props.annotations === deidentificationStates.EMPTY ||
+      props.annotations === deidentificationStates.LOADING ||
+      props.annotations === deidentificationStates.ERROR) {
+    allAnnotations = [];
   } else {
     allAnnotations = types.map(
       (type) => props.annotations[type.key].map((annotation, index) => {
-        return {type: type.name, id: String(type.type)+'_'+String(index), ...annotation};
-      })
+        return {
+          type: type.name,
+          id: String(type.type)+'_'+String(index),
+          ...annotation,
+        };
+      }),
     ).flat();
   }
 
   const columns = [
-    {field: 'id', headerName: "ID", hide: true},
-    {field: 'type', headerName: "Type", width: 125},
-    {field: 'text', headerName: "Text", width: 130},
-    {field: 'start', headerName: "Start", width: 90},
-    {field: 'length', headerName: "Length", width: 100},
-    {field: 'confidence', headerName: "Confidence", width: 130}
-  ]
+    {field: 'id', headerName: 'ID', hide: true},
+    {field: 'type', headerName: 'Type', width: 125},
+    {field: 'text', headerName: 'Text', width: 130},
+    {field: 'start', headerName: 'Start', width: 90},
+    {field: 'length', headerName: 'Length', width: 100},
+    {field: 'confidence', headerName: 'Confidence', width: 130},
+  ];
 
   return (
-    <Paper elevation={3} style={{ height: "400px" }}>
+    <Paper elevation={3} style={{height: '400px'}}>
       <DataGrid rows={allAnnotations} columns={columns}/>
     </Paper>
   );
 }
+
+AnnotationView.propTypes = {
+  annotations: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+  ]),
+};
+
+export default AnnotationView;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -13,7 +13,7 @@ ReactDOM.render(
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
